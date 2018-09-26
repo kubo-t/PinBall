d54e55d768f73cf823f1ef05d558c5506ab070a9
kubotakuya <kbtk8427@gmail.com>
2018-09-26 21:44:53 +0900

    Lesson5　課題

---
+++ Assets/BrightnessLegurator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessLegurator : MonoBehaviour {
+
+	Material myMaterial;
+
+	public GameObject scoreText;
+
+	private float minEmission = 0.3f;
+	private float magEmission = 2.0f;
+
+	private int degree = 0;
+	private int speed = 10;
+
+	Color defaultColor = Color.white;
+
+	// Use this for initialization
+	void Start () {
+		if(tag == "SmallStarTag"){
+			this.defaultColor = Color.white;
+		}else if(tag == "LargeStarTag"){
+			this.defaultColor = Color.yellow;
+		}else if(tag == "SmallCloudTag" || tag == "LargeCloudTag"){
+			this.defaultColor = Color.red;
+		}
+
+		this.myMaterial = GetComponent<Renderer> ().material;
+		myMaterial.SetColor ("_EmissionColor", this.defaultColor*minEmission);
+
+		scoreText = GameObject.Find ("Score");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.degree >= 0){
+			Color emissionColor = this.defaultColor * (this.minEmission + Mathf.Sin (this.degree * Mathf.Deg2Rad) * this.magEmission);
+		
+			myMaterial.SetColor ("_EmissionColor", emissionColor);
+			this.degree -= this.speed;
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		this.degree = 100;
+
+		if(tag == "LargeCloudTag" || tag == "LargeStarTag"){
+			this.scoreText.GetComponent<ScoreController> ().score += 20;
+		}else if(tag == "SmallStarTag" || tag == "SmallCloudTag"){
+			this.scoreText.GetComponent<ScoreController> ().score += 10;
+		}
+	}
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+	private HingeJoint myHingeJoint;
+
+	private float defaultAngle = 20;
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		this.myHingeJoint = GetComponent<HingeJoint> ();
+
+		SetAngle (this.defaultAngle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		MouseInput ();
+		TouchInput ();
+	}
+
+	public void SetAngle(float angle){
+		JointSpring jointspr = this.myHingeJoint.spring;
+		jointspr.targetPosition = angle;
+		this.myHingeJoint.spring = jointspr;
+	}
+
+	void MouseInput(){
+		if(Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag"){
+			SetAngle (this.flickAngle);
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag"){
+			SetAngle (this.flickAngle);
+		}
+		if(Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag"){
+			SetAngle (this.defaultAngle);
+		}
+		if(Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag"){
+			SetAngle (this.defaultAngle);
+		}
+	}
+
+	void TouchInput(){
+		if(Input.touchCount > 0){
+			for(int i = 0; i < Input.touchCount; i++){
+
+				Touch t = Input.GetTouch (i);
+
+				if(t.phase == TouchPhase.Began && t.position.x > Screen.width/2 && tag == "RightFripperTag"){
+					SetAngle (this.flickAngle);
+				}
+				if(t.phase == TouchPhase.Ended && t.position.x > Screen.width/2 && tag == "RightFripperTag"){
+					SetAngle (this.defaultAngle);
+				}
+				if(t.phase == TouchPhase.Began && t.position.x < Screen.width/2 && tag == "LeftFripperTag"){
+					SetAngle (this.flickAngle);
+				}
+				if(t.phase == TouchPhase.Ended && t.position.x < Screen.width/2 && tag == "LeftFripperTag"){
+					SetAngle (this.defaultAngle);
+				}
+
+
+			}
+		}
+
+	}
+}
